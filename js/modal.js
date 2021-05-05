604fb482b84b0d1078537e2f175a2aa477c67893
martinbolsnes <69841820+martinbolsnes@users.noreply.github.com>
2021-05-05 15:04:03 +0200

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -6,11 +6,11 @@ img.onclick = function () {
   modal.style.display = 'block';
 };
 
-window.onclick = function (event) {
+/* window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = 'none';
   }
-};
+}; */
 
 span.onclick = function () {
   modal.style.display = 'none';
